f19c1ed57127027417a7aecf69faf80e0175478c
Mikhail9030 <gulaevmihail112@gmail.com>
2021-02-14 22:40:57 +0300

    Commit2: Исправлены замечания

--- src/test/java/ru/netology/unit/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/unit/CashbackHackServiceTest.java
@@ -1,35 +1,26 @@
 package ru.netology.unit;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
-import static org.testng.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
-    @org.testng.annotations.Test
+    @Test
     public void shouldCalculateIfAmountCorrect() {
         int amount = 5700;
         int actual = service.remain(amount);
         int expected = 300;
-        assertEquals(actual,expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
-    public void shouldCalculateIfAmountInCorrect() {
-        int amount = 5000;
-        int actual = service.remain(amount);
-        int expected = 0;
-        assertEquals(actual,expected);
-    }
-
-    @org.testng.annotations.Test
+    @Test
     public void shouldCalculateIfAmountZero() {
         int amount = 0;
         int actual = service.remain(amount);
         int expected = 1000;
-        assertEquals(actual,expected);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -37,6 +28,14 @@ public class CashbackHackServiceTest {
         int amount = 1000;
         int actual = service.remain(amount);
         int expected = 0;
-        assertEquals(actual,expected);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculateIfAmountIncorrect() {
+        int amount = -50000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
     }
 }
